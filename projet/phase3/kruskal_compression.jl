e660d8f03e11287dee58025044215f20ec1b267b
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-10-25 17:01:26 -0400

    Update kruskal_compression.jl

--- projet/phase3/kruskal_compression.jl
+++ projet/phase3/kruskal_compression.jl
@@ -4,29 +4,18 @@ include(joinpath(here,"..","phase1","node.jl"))
 include(joinpath(here,"..","phase1","edge.jl"))
 include(joinpath(here,"..","phase1","graph.jl"))
 
-import Base.union!
-
-"""
-Fonction renvoyant un booléen true si les deux noeuds de l'arête
+"""Renvoie un booléen true si les deux noeuds de l'arête
  edge sont dans le même ensemble connexe et false autrement
-
 """
-function connex(edge::Edge{T},dict::Dict{String,String}) where T
-        #On récupère dans node1 et node2 les deux noeuds contenus dans l'arête edge
-    node1=name(data(edge)[1])
-    node2=name(data(edge)[2])
-        #Si ces deux noeuds ont la même racine, ils sont dans le même ensemble connexe, sinon non
-    dict[node1]==dict[node2]
+function connex_compression(node1::Node{T}, node2::Node{T},dict::Dict{String,String}) where T
+    dict[name(node1)]==dict[name(node2)]
 end
 
+
+"""Réalise l'union de deux arborescences dans la forêt
+      représentée par le dictionaire dict
 """
-Fonction réalisant l'union de deux arborescences, par l'arête edge,
- et dans la forêt représentée par le dictionaire dict
- """
-function union_compression!(edge::Edge,dict::Dict{String,String})
-    #On récupère dans node1 et node2 les deux noeuds contenus dans l'arête edge
-    node1=data(edge)[1]
-    node2=data(edge)[2]
+function union_compression!(node1::Node{T}, node2::Node{T}, dict::Dict{String,String}) where T
     root1=dict[name(node1)]
     root2=dict[name(node2)]
     for (key,val) in dict
@@ -37,11 +26,9 @@ function union_compression!(edge::Edge,dict::Dict{String,String})
 end
 
 
+"""Renvoie un arbre de recouvrement de coût minimal associé au
+ graphe G, en utilisant l'algorithme de Kruskal et l'union par compression
 """
-Fonction renvoyant un arbre de recouvrement de coût minimal associé au
- graphe G, en utilisant l'algorithme de Kruskal
-
- """
 function kruskal_compression(G::Graph)
      E = edges(G)
      #Tri des arêtes par poids
@@ -52,11 +39,11 @@ function kruskal_compression(G::Graph)
      G_construction=Graph("Arbre", nodes(G), Edge{Vector{Float64}}[])
      for e in E
          #Si les deux noeuds de l'arête e ne sont pas dans le même ensemble connexe
-        if connex(e,parents)==false
+        if connex_compression(data(e)[1],data(e)[2],parents)==false
             #On ajoute cette arête au graphe de construction
             add_edge!(G_construction,e)
             #On ajoute cette arête à la forêt d'arborescence
-            union_compression!(e,parents)
+            union_compression!(data(e)[1],data(e)[2],parents)
         end
     end
     #Le graphe de construction obtenu est un arbre de recouvrement de G
