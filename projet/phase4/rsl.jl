5ead612d1bed35b5fede7525466aa458d708913c
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-11-18 15:53:03 -0500

    Update rsl.jl

--- projet/phase4/rsl.jl
+++ projet/phase4/rsl.jl
@@ -1,20 +1,30 @@
-"""Renvoie une tournée en utilisant l'algorithme de RSL"""
+#include("primNew.jl")
+ """Renvoie une tournée en utilisant l'algorithme de RSL"""
 
-function rsl(G::Graph{T},root=nodes(G)[1]::Node{T}) where T
-    tree = prim(G, root)
-    cycle = Graph("cycle", [root], Edge{T}[])
-    #Création d'un dictionaire associant à chaque couple de noeuds reliés par une arête le poids de cette arête
-    weights = weight_graph(G)
-    n = length(nodes(tree))
-    nodesTree_copy = copy(nodes(tree))
-    node1 = popfirst!(nodesTree_copy)
-    while !isempty(nodesTree_copy)
-        node2 = popfirst!(nodesTree_copy)
-        add_node!(cycle, node2)
-        add_edge!(cycle, Edge((node1, node2), weights[(name(node1),name(node2))]))
-        node1 = node2
-    end
-    add_edge!(cycle, Edge((nodes(tree)[n], nodes(tree)[1]), weights[(name(nodes(tree)[n]),name(nodes(tree)[1]))] ))
-    return cycle
+ function rsl(G::Graph{T},root=nodes(G)[1]::Node{T}) where T
+     n=length(nodes(G))
+     tree, parents = prim(G, root)
+     children = Dict(node => Node{T}[] for node in nodes(G))
 
- end
+     for (child,parent) in parents
+         push!(children[parent],child)
+     end
+     nodesTree_copy = pre_order!(root, children, parents,nodes(G))
+
+     cycle = Graph("cycle", [root], Edge{T}[])
+
+     #Création d'un dictionaire associant à chaque couple de noeuds reliés par une arête le poids de cette arête
+     weights = weight_graph(G)
+     n = length(nodes(tree))
+
+     node1 = popfirst!(nodesTree_copy)
+     while !isempty(nodesTree_copy)
+         node2 = popfirst!(nodesTree_copy)
+         add_node!(cycle, node2)
+         add_edge!(cycle, Edge((node1, node2), weights[(name(node1),name(node2))]))
+         node1 = node2
+     end
+     add_edge!(cycle, Edge((nodes(tree)[n], nodes(tree)[1]), weights[(name(nodes(tree)[n]),name(nodes(tree)[1]))] ))
+     return cycle,tree
+
+  end
