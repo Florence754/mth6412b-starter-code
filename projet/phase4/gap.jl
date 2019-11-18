92a73ebb0bb457679b6eb4513004ccf063445261
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-11-18 18:34:38 -0500

    Create gap.jl

---
+++ projet/phase4/gap.jl
@@ -0,0 +1,17 @@
+"""Calcul du gap entre la solution actuelle (le cycle) et la borne inférieure définie par le 1-arbre
+"""
+function eval_gap(cycle::Graph{T},onetree::Graph{T}, node_weights::Dict, weights::Dict) where T
+     cycle_cost = 0.0
+     for edge in edges(cycle)
+         cycle_cost += weights[(name(data(edge)[1]), name(data(edge)[2]))]
+     end
+     onetree_cost = length(onetree)
+     sum=0.0
+
+     for node in nodes(onetree)
+         sum += 2*node_weights[name(node)]
+     end
+
+     gap = cycle_cost - (onetree_cost - sum)
+     return gap, cycle_cost
+ end
