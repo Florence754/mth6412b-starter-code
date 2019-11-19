8404e927970e9d0ea91dde596b1a40065e94cee5
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-11-18 20:26:00 -0500

    Update onetree.jl

--- projet/phase4/onetree.jl
+++ projet/phase4/onetree.jl
@@ -1,49 +1,41 @@
 here = @__DIR__
-include("prim_New.jl")
-include(joinpath(here,"..","phase3","kruskal.jl"))
+  #include("prim_New.jl")
+  include(joinpath(here,"..","phase3","kruskal.jl"))
 
-"""Construit un 1_arbre de noeud racine 'root' en utilisant la methode Kruskal ou prim
- 'method' = 1 pour kruskal et 'method' = 2 pour prim  """
-function onetree(G::Graph{T}, root::Node{T}, method = 1::Set([1,2])) where T
-        edges_vector = copy(edges(G))
-        nodes_vector = copy(nodes(G))
-        graph=Graph("",nodes_vector,edges_vector)
-        delete_node!(graph,root)
-        #Création d'un arbre de coût minmal avec tous les noeuds sauf root
-        if method == 1
-                onetree = kruskal(graph)
-        elseif method == 2
-                onetree = prim(G)
-        end
+  """Construit un 1_arbre de noeud racine 'root' en utilisant l'algorithme de Prim  """
+  function onetree(G::Graph{T}, root::Node{T}) where T
+          edges_vector = copy(edges(G))
+          nodes_vector = copy(nodes(G))
+          graph = Graph("",nodes_vector,edges_vector)
+          delete_node!(graph,root)
+          #Création d'un arbre de coût minmal avec tous les noeuds sauf root
+          onetree, parents, tree_root = prim(graph)
+          weights = weight_graph(G)
+          #Recherche des deux arêtes de poids minimal partant de root
+          #Première arête
+          minweight1 = Inf
+          n1 = root
+          for node in nodes(onetree)
+                  if weights[(name(node),name(root))] < minweight1
+                      minweight1 = weights[(name(node),name(root))]
+                      n1 = node
+                  end
+          end
+          edge1 = Edge((n1,root),minweight1) #Création de la première arête de poids minimal reliant root à onetree
+          #Deuxième arête
+          minweight2 = Inf
+          n2 = root
+          for node in nodes(onetree)
+                  if weights[(name(node),name(root))] < minweight2 && node != n1
+                      minweight2 = weights[(name(node),name(root))]
+                      n2 = node
+                  end
+          end
+          edge2 = Edge((n2,root),minweight2) #Création de la deuxième arête de poids minimal reliant root à onetree
 
-        weights = weight_graph(G)
-        minweight1 = Inf
-        n1 = root
-        n2 = root
-        for node in nodes(onetree)
-            if in_edges(edges(G),Edge((root,node),0)) == true
-                if weights[(name(node),name(root))] < minweight1
-                    minweight1 = weights[(name(node),name(root))]
-                    n1 = node
-                end
-        end
-        end
-
-        edge1 = Edge((n1,root),minweight1)
-
-        minweight2 = Inf
-        for node in nodes(onetree)
-            if in_edges(edges(G),Edge((root,node),0)) == true
-                if weights[(name(node),name(root))] < minweight2 && node != n1
-                    minweight2 = weights[(name(node),name(root))]
-                    n2 = node
-                end
-        end
-        end
-        edge2 = Edge((n2,root),minweight2)
-
-        add_node!(onetree,root)
-        add_edge!(onetree,edge1)
-        add_edge!(onetree,edge2)
-        return onetree
-end
+          #Ajout de root au 1-arbre
+          add_node!(onetree,root)
+          add_edge!(onetree,edge1)
+          add_edge!(onetree,edge2)
+          return onetree, parents, tree_root
+  end
