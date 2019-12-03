0adc3a301b7c0793cafdcc80a34b5ec9d2efb912
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-12-02 19:48:48 -0500

    Create read_graph1.jl

---
+++ projet/phase5/read_graph1.jl
@@ -0,0 +1,24 @@
+
+include(joinpath(@__DIR__,"..","phase1","read_stsp.jl"))
+"""read_graph_stsp(filename)
+
+Construct a graph from given stsp file.
+"""
+function read_graph_stsp(filename::String)
+    header = read_header(filename)
+    nom = header["NAME"]
+    graph_edges = read_edges(header,filename)
+    G = Graph(nom, Node{Int}[], Edge{Int}[])
+
+    # On ajoute les noueds un par un au graphe G
+    graph_nodes = read_nodes(header, filename)
+    for (i, nodes) in graph_nodes
+        add_node!(G, Node(string(i), nodes))
+    end
+    # On ajoute les arêtes au graphe G
+    for e in graph_edges
+        add_edge!(G, Edge((Node(string(e[1]), graph_nodes[e[1]]), Node(string(e[2]), graph_nodes[e[2]])), e[3]))
+    end
+    # sort!(nodes(G),by=data)
+    return G
+end
